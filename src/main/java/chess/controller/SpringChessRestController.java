c800f27a6b627fb37da58c215175527bfc712ec7
sakjung <ssangyu123@gmail.com>
2021-05-02 13:29:04 +0900

    test: controller에 로그가 제대로 기록되는지 테스트 하는 코드 추가

--- src/main/java/chess/controller/SpringChessRestController.java
+++ src/main/java/chess/controller/SpringChessRestController.java
@@ -2,13 +2,20 @@ package chess.controller;
 
 import chess.dto.DeleteDto;
 import chess.service.SpringChessService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringChessRestController {
+    private static final Logger log = LoggerFactory.getLogger("console");
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     private final SpringChessService springChessService;
 
     public SpringChessRestController(SpringChessService springChessService) {
@@ -21,5 +28,19 @@ public class SpringChessRestController {
         DeleteDto deleteDto = new DeleteDto(roomName, true);
 
         return ResponseEntity.ok().body(deleteDto);
+    }
+
+    @GetMapping("/file")
+    public String logbackFileTest() {
+        fileLogger.info("파일로깅 입니다.");
+        fileLogger.debug("디버그 파일로깅 입니다.");
+        return "파일 로깅 테스트 페이지 입니다";
+    }
+
+    @GetMapping("/json")
+    public String logbackJsonTest() {
+        jsonLogger.info("제이슨 로깅입니다.");
+        jsonLogger.debug("디버그 제이슨 로깅입니다.");
+        return "제이슨 로깅 테스트 페이지 입니다";
     }
 }
--- src/test/java/LoggingTest.java
+++ src/test/java/LoggingTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class LogginTest {
+import chess.controller.SpringChessController;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingTest {
+    private static final Logger log = LoggerFactory.getLogger(SpringChessController.class);
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
+    @Test
+    void loggingLearningTest() {
+        log.error("An ERROR Message");
+        fileLogger.info("파일 로깅 입니다.");
+    }
 }
